83111a1cce26e0814fd82aa0398c7cfdc6aed2c2
shiyolyl <yl2014nyqd@163.com>
2019-02-25 00:49:46 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Button } from 'antd'
 import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Button type="primary">Button</Button>
+        {/*
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -20,6 +24,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        */}
       </div>
     );
   }
